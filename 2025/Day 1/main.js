cb7c16e51a12111d9c7b054153e20d802a212602
IweZix <lucanclv2@gmail.com>
2025-12-03 20:34:29 +0100

    feat: day 1 part 1

---
+++ 2025/Day 1/main.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+function countZerosFromFile() {
+  const data = fs.readFileSync("input.txt", "utf8").trim().split("\n");
+
+  let start = 50;
+  let cpt = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const line = data[i].trim();
+    if (!line) continue;
+
+    const direction = line.charAt(0);
+    const value = parseInt(line.slice(1), 10);
+
+    if (direction === "L") {
+      start = start - value;
+
+      while (start < 0) {
+        start += 100;
+      }
+
+      if (start === 0) cpt++;
+    } else {
+      start = start + value;
+
+      while (start >= 100) {
+        start -= 100;
+      }
+
+      if (start === 0) cpt++;
+    }
+  }
+
+  return cpt;
+}
+
+console.log(countZerosFromFile());
